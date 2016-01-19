45eddc9fde827a081ac0c40050b51df21e9df1da
Max Wheeler <max@makenosound.com>
2016-01-19 16:51:10 +1100

    Add a `section` to the demo form.

--- lib/forms/demo.rb
+++ lib/forms/demo.rb
@@ -2,8 +2,13 @@ require "formalist"
 
 module Forms
   class Demo < Formalist::Form
-    field :name, type: "string", label: "Name"
-    field :title, type: "string", display: "select", label: "Title", hint: "Nudge, nudge, wink, wink", option_values: ["Lord", "Lady", "Wing Commander"]
-    field :published, type: "bool", question_text: "Publish this person?"
+    section :main do |section|
+      section.field :name, type: "string", label: "Name"
+      section.field :title, type: "string", display: "select", label: "Title", hint: "Nudge, nudge, wink, wink", option_values: ["Lord", "Lady", "Wing Commander"]
+      section.field :age, type: "string", display: "select", label: "age", hint: "Nudge, nudge, wink, wink", option_values: ["0-10", "10-50", "50-51"]
+    end
+    section :meta do |section|
+      section.field :published, type: "bool", question_text: "Publish this person?"
+    end
   end
 end
