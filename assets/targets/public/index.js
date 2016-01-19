49d397c116fbc82f6c98d56fd8d02679db76d7f9
Max Wheeler <max@makenosound.com>
2016-01-19 15:53:50 +1100

    Create the base formalist-standard-react consuming app wrapper.

--- assets/targets/public/index.js
+++ assets/targets/public/index.js
@@ -1,1 +1,62 @@
-// JS goes here.
+import domReady from 'domready'
+import viewloader from 'viewloader'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Immutable from 'immutable'
+import template from 'formalist-standard-react'
+
+/**
+ * Simple wrapper to create the form outer
+ */
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    let form = this.props.form
+    this.state = {
+      formState: form.store.getState()
+    }
+  }
+
+  componentWillMount() {
+    let form = this.props.form
+    form.store.subscribe(() => {
+      this.setState({
+        formState: form.store.getState()
+      })
+    })
+  }
+
+  render() {
+    let form = this.props.form
+    return (
+      <div className="appWrapper">
+        <form method="post" action="">
+          {form.render()}
+          <input name="data" type="hidden" value={JSON.stringify(this.state.formState.toJS())}/>
+          <button>Submit form</button>
+        </form>
+        <pre><code>{JSON.stringify(this.state.formState.toJS(), null, 2)}</code></pre>
+      </div>
+    )
+  }
+}
+
+/**
+ * Object for holding our `viewloader` views
+ */
+const views = {
+  form: (el, ast) => {
+    let data = JSON.parse(ast)
+    let configuredTemplate = template()
+    let form = configuredTemplate(data)
+    ReactDOM.render(<App form={form} />, el);
+  }
+}
+
+/**
+ * Kick things off on domReady
+ */
+domReady(() => {
+  viewloader.execute(views);
+})
+
