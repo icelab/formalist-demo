9e3360cb6e22d05fc0c2756dc81b636ae7620454
Max Wheeler <max@makenosound.com>
2016-01-19 15:54:03 +1100

    Make the form slightly fancier.

--- lib/forms/demo.rb
+++ lib/forms/demo.rb
@@ -2,6 +2,8 @@ require "formalist"
 
 module Forms
   class Demo < Formalist::Form
-    field :title, type: "string"
+    field :name, type: "string", label: "Name"
+    field :title, type: "string", display: "select", label: "Title", hint: "Nudge, nudge, wink, wink", option_values: ["Lord", "Lady", "Wing Commander"]
+    field :published, type: "bool", question_text: "Publish this person?"
   end
 end
