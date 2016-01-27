14bf8037928eea18321c85edf922d3e43d4af785
Tim Riley <tim@openmonkey.com>
2016-01-27 12:46:54 +1100

    Include age in validations

--- lib/validation/demo_schema.rb
+++ lib/validation/demo_schema.rb
@@ -5,6 +5,7 @@ module Validation
   class DemoSchema < Dry::Validation::Schema::Form
     key(:name, &:filled?)
     key(:title) { |title| title.inclusion?(["Lord", "Lady", "Wing Commander"]) }
+    key(:age, &:filled?)
     key(:published, &:bool?)
   end
 end
