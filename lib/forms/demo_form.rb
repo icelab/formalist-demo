ff862962c6e2f587f8d7b6d56b7b912cebb70669
Max Wheeler <max@makenosound.com>
2016-03-16 10:44:42 +1100

    Change form to the reference data we’re using elsewhere.

--- lib/forms/demo_form.rb
+++ lib/forms/demo_form.rb
@@ -5,117 +5,73 @@ require "validation/demo_schema"
 module Forms
   DemoForm = Class.new(Formalist::Form) do
     define do
-      section :date do
-        date_field :date_default, label: "Date (default)"
+      text_field :text_field,
+        label: "Text field",
+        placeholder: "Text field placeholder",
+        hint: "Text field hint"
+
+      number_field :number_field,
+        label: "Number field",
+        placeholder: "Number field placeholder",
+        step: 0.5,
+        min: 10,
+        max: 20
+
+      check_box :check_box,
+        label: "Checkbox",
+        question_text: "Checkbox question?"
+
+      select_box :select_box,
+        label: "Select box",
+        options: [["1", "One"], ["2", "Two"], ["3", "Three"]]
+
+      radio_buttons :radio_buttons,
+        label: "Radio buttons",
+        options: [["1", "One"], ["2", "Two"], ["3", "Three"]]
+
+      text_area :text_area,
+        label: "Text area",
+        placeholder: "Text area placeholder"
+
+      date_field :date_field,
+        label: "Date field"
+
+      date_time_field :date_time_field,
+        label: "Date-time field"
+
+      section :section, label: "Section label" do
+        text_field :section_text_field,
+          label: "Section text field"
+        number_field :section_number_field,
+          label: "Section number field"
       end
 
-      section :datetime do
-        date_time_field :date_time_default, label: "Date-time (default)"
+      group :group, label: "Group label" do
+        text_field :group_text_field,
+          label: "Group text field"
+        number_field :group_number_field,
+          label: "Group number field"
       end
 
-      section :string do
-        text_field :string_default,
-          label: "String (default)",
-          placeholder: "Default string type",
-          validation: {filled?: true}
-
-        text_field :string_password,
-          label: "String (password)",
-          placeholder: "Secret squirrel",
-          password: true,
-          validation: {filled?: true, min_size?: 5}
-
-        text_field :string_code,
-          label: "String (default) with code",
-          placeholder: "Enter the matrix",
-          code: true
-
-        select_box :string_select,
-          label: "String (select)",
-          placeholder: "Select your favourite number",
-          options: [["1", "One"], ["2", "Two"], ["3", "Three"]]
-
-        radio_buttons :string_radio,
-          label: "String (radio)",
-          options: [["1", "One"], ["2", "Two"], ["3", "Three"]]
-
-        text_area :string_textarea,
-          label: "String (textarea)",
-          placeholder: "Enter some text in a larger area",
-          hint: "You can enter text over more than one line."
-
-        text_area :string_huge,
-          label: "String (textarea) huge",
-          placeholder: "Enter some text in a larger area",
-          hint: "You can enter text over more than one line.",
-          box_size: "xlarge"
+      many :many do
+        text_field :many_text_field,
+          label: "Many text field"
+        date_field :many_date_field,
+          label: "Many date field"
       end
 
-      section :int do
-        number_field :int_default,
-          label: "Int (default)",
-          placeholder: "Default int input"
-
-        number_field :int_min,
-          label: "Int (default) with min 10",
-          placeholder: "Default int input",
-          min: 10
-
-        number_field :int_max,
-          label: "Int (default) with max 10",
-          placeholder: "Default int input",
-          max: 10
-
-        number_field :int_step,
-          label: "Int (default) with step 5",
-          placeholder: "Default int input",
-          step: 5
-
-        select_box :int_select,
-          label: "Int (select)",
-          placeholder: "Select your favourite number",
-          options: [["1", "One"], ["2", "Two"], ["3", "Three"]]
-
-        radio_buttons :int_radio,
-          label: "Int (radio)",
-          options: [["1", "One"], ["2", "Two"], ["3", "Three"]]
+      attr :attr do
+        text_field :attr_text_field,
+          label: "Attr text field"
+        date_field :attr_date_field,
+          label: "Attr date field"
       end
 
-      section :float do
-        number_field :float_default,
-          label: "Float (default)",
-          placeholder: "Default float input"
-
-        number_field :float_min,
-          label: "Float (default) with min 10",
-          placeholder: "Default float input",
-          min: 10
-
-        number_field :float_max,
-          label: "Float (default) with max 10",
-          placeholder: "Default float input",
-          max: 10
-
-        group do
-          number_field :float_step,
-            label: "Float (default) with step 0.025",
-            placeholder: "Default float input", step: 0.025
-
-          select_box :float_select,
-            label: "Float (select)",
-            placeholder: "Select your favourite number",
-            option: [["1.1", "1.1"], ["2.4", "2.4"], ["3.14159265359", "π"]]
-        end
-
-        radio_buttons :float_radio,
-          label: "Float (radio)",
-            option: [["1.1", "1.1"], ["2.4", "2.4"], ["3.14159265359", "π"]]
-      end
-
-      section :bool do
-        check_box :bool_default,
-          label: "Bool (default)",
-          question_text: "Do you like me?"
+      compound_field do
+        text_field :compound_field_text_field,
+          label: "Compound text field"
+        date_field :compound_field_date_field,
+          label: "Compound date field"
       end
     end
   end.new
