a897ce83b8bbcc9fe727397390cd27939bbc4570
Tim Riley <tim@openmonkey.com>
2016-04-04 17:15:38 +1000

    Return the upload URL in the presign response

--- web/routes/uploads.rb
+++ web/routes/uploads.rb
@@ -6,11 +6,13 @@ module FormalistDemo
     route "uploads" do |r|
       r.on "presign" do
         r.post do
+          attache_host = FormalistDemo::Container.config.options.attache_host
           secret_key = FormalistDemo::Container.config.options.attache_secret_key
           uuid = SecureRandom.uuid
           expiration = (Time.now + 60*60*3).to_i # 3 hours from now
 
           {
+            url: "#{attache_host}/upload",
             uuid: uuid,
             expiration: expiration,
             hmac: OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), secret_key, "#{uuid}#{expiration}"),
